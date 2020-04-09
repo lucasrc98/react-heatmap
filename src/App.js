3e19e4282e6334634e3eef5de11f936f3dd96fba
Lucas Rodrigues <lucasrchaves1@gmail.com>
2020-04-09 14:45:12 -0300

    Melhorando exemplo usando imagem de campo de futebol

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import h337 from "heatmap.js";
 
@@ -18,42 +18,43 @@ export default function App() {
     var points = [];
     var max = 0;
     var min = 0;
-    var width = 1276;
-    var height = 86;
+    var width = 700;
+    var height = 800;
     var len = 200;
 
     while (len--) {
-     var val = Math.floor(Math.random()*100);
-     max = Math.max(max, val);
+      var val = Math.floor(Math.random() * 100);
+      max = Math.max(max, val);
 
-     var point = {
-      x: Math.floor(Math.random()*width),
-      y: Math.floor(Math.random()*height),
-      value: val
-     };
+      var point = {
+        x: Math.floor(Math.random() * width),
+        y: Math.floor(Math.random() * height),
+        value: val
+      };
 
-     // Adicionando os pontos: x, y e o valor de tal ponto
-     points.push(point);
-   }
+      // Adicionando os pontos: x, y e o valor de tal ponto
+      points.push(point);
+    }
     ///////////////////////////////////////////////////////////////////////////////
 
-   // formato de dados do heatmap
-  var data = {
-    max: max,
-    min: min,
-    data: points
-  };
+    // formato de dados do heatmap
+    var data = {
+      max: max,
+      min: min,
+      data: points
+    };
 
-  // se você tiver um conjunto de pontos de dados, use sempre setData em vez de addData
-  // para inicialização de dados
+    // se você tiver um conjunto de pontos de dados, use sempre setData em vez de addData
+    // para inicialização de dados
     heatmapInstance.setData(data);
- })
+  })
 
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing see some magic happen!</h2>
-    </div>
+    <>
+      <div className="App">
+          <img src="https://thumbs.dreamstime.com/b/campo-de-futebol-verde-da-vista-superior-43683754.jpg"></img>
+      </div>
+    </>
   );
 }
 
